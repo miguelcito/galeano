61aad5c3c8670687ad9096ad44d4930e12935335
miguel <espinoza@12>
2016-03-03 21:27:32 -0500

    luz galeano jefa

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>luz galeno y miguel espinoza </title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<h>hola amio miguel</h>
+<<img src="daysi.gif" alt="">
+<h>hola amia luz como estas</h>
+<<img src="mmm.gif" alt="">
+<h>yo todo bn gracias amio </h>
+	
+</body>
+</html>
